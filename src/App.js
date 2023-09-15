c83f6802f778f8b5588be7cc33360291e60a199c
Lucy W. Mwangi <100368317+lucy-sees@users.noreply.github.com>
2023-09-15 19:04:46 +0300

    Update App component

--- src/App.js
+++ src/App.js
@@ -2,22 +2,14 @@ import './App.css';
 import React from 'react';
 import Calc from './components/Calculator';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <Calc />
-        </header>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Calc />
+      </header>
+    </div>
+  );
 }
 
 export default App;
