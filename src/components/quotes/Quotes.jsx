738b8379514ea561276eabe2260a7162139e4299
Lucy W. Mwangi <100368317+lucy-sees@users.noreply.github.com>
2023-09-16 17:47:26 +0300

    Remove third-party library

--- src/components/quotes/Quotes.jsx
+++ src/components/quotes/Quotes.jsx
@@ -1,67 +1,67 @@
 import React, { useEffect, useState } from 'react';
-import Swal from 'sweetalert2';
 import './quotes.css';
 
-const ShowQuotes = () => {
+const apiUrl = process.env.REACT_APP_API_URL;
+const apiKey = process.env.REACT_APP_API_KEY;
+
+const Quotes = () => {
   const [quotes, setQuotes] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  const getData = async () => {
+    try {
+      setIsLoading(true);
+      const response = await fetch(apiUrl, {
+        method: 'GET',
+        headers: {
+          'X-Api-Key': apiKey,
+          'Content-Type': 'application/json',
+        },
+      });
+      const data = await response.json();
+      setQuotes(data[0]);
+      setIsLoading(false);
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+    } catch (error) {
+      setIsError(true);
+    }
+  };
 
   useEffect(() => {
-    const apiUrl = process.env.REACT_APP_API_URL;
-    const apiKey = process.env.REACT_APP_API_KEY;
-
-    Swal.fire({
-      title: 'Fetching Quotes...',
-      html: '<div class="sweet-alert-loading-spinner"><div class="spinner"></div></div>',
-      allowOutsideClick: false,
-      showConfirmButton: false,
-    });
-
-    fetch(apiUrl, {
-      method: 'GET',
-      headers: {
-        'X-Api-Key': apiKey,
-      },
-    }).then((response) => response.json()).then((responseData) => {
-      Swal.close();
-      setQuotes(responseData);
-    }).catch((error) => {
-      Swal.close();
-      Swal.fire({
-        icon: 'error',
-        title: 'Error',
-        text: 'There was a problem with the fetch operation',
-      });
-      throw new Error('Something wrong happened', error);
-    });
+    getData();
   }, []);
 
+  const message = isError ? 'There must be some error' : 'Loading...';
+  if (isLoading || isError) {
+    return <p className="message">{message}</p>;
+  }
   return (
     <div className="container">
       <div className="quotesDiv">
         <h2>Quotes</h2>
         <ul className="quotesList">
-          {quotes.map((quote, index) => (
-            <li key={quote.id}>
-              <span>
-                {index + 1}
-                {'. '}
-                {quote.quote}
-              </span>
-              <span>
-                Author:
-                {' '}
-                {quote.author}
-              </span>
-              <span>
-                Category:
-                {quote.category}
-              </span>
-            </li>
-          ))}
+          <li key={quotes.id}>
+            <span>
+              {'. '}
+              {quotes.quote}
+            </span>
+            <span>
+              Author:
+              {' '}
+              {quotes.author}
+            </span>
+            <span>
+              Category:
+              {quotes.category}
+            </span>
+          </li>
         </ul>
       </div>
     </div>
   );
 };
 
-export default ShowQuotes;
+export default Quotes;
