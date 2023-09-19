24b9bad1b34625d8dd110db7e2fe7520d2e2af90
Lucy W. Mwangi <100368317+lucy-sees@users.noreply.github.com>
2023-09-19 17:23:53 +0300

    Add Home and Quotes pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
 import React from 'react';
 import Calc from './components/Calculator';
+import Home from './components/Home';
+import Quote from './components/Quotes';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Calc />
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/quote" element={<Quote />} />
+      <Route path="/calculator" element={<Calc />} />
+    </Routes>
   );
 }
 
 export default App;
+
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+
+//     this.state = {};
+//   }
+
+//   render() {
+//     return (
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/quote" element={<Quote />} />
+//         <Route path="/calculator" element={<Calc />} />
+//       </Routes>
+//     );
+//   }
+// }
