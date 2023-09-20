8052e2d3839b7f7df98040ee7d65a2f0763facc5
Lucy W. Mwangi <100368317+lucy-sees@users.noreply.github.com>
2023-09-20 12:35:17 +0300

    Import nav component

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Nav from './components/Navigation';
+import Nav from './components/navigation/Navigation';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
